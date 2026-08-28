9f63ec5a3136884c70e5996492c60b4f398ef6eb
webst <luxestoneinteriors@gmail.com>
2026-08-28 18:51:23 -0400

    fix type error

--- lib/store/materials.ts
+++ lib/store/materials.ts
@@ -19,7 +19,7 @@ export async function getMaterialBySlug(slug: string): Promise<MaterialPage | un
 }
 
 export interface MaterialInput {
-  name: string;
+  MaterialPage["name"];
   intro: string;
   characteristics: string[];
   applications: MaterialPage["applications"];
